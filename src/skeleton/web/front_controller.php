f944f4c1344b23db0a287df35fddd31646ff2b76
Fabien Potencier <fabien.potencier@gmail.com>
2010-11-06 16:02:52 +0100

    updated vendors

--- src/skeleton/web/front_controller.php
+++ src/skeleton/web/front_controller.php
@@ -2,5 +2,7 @@
 
 require_once __DIR__.'/../{{ application }}/{{ class }}Kernel.php';
 
+use Symfony\Component\HttpFoundation\Request;
+
 $kernel = new {{ class }}Kernel('prod', false);
-$kernel->handle()->send();
+$kernel->handle(new Request())->send();
